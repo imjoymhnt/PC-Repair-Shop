135a37c0c3af5c4f14722c4570e83269aad1c73c
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-01-29 21:40:05 +0530

    add createService form and saving the data into mongodb using mongoose

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/serviceDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const serviceSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  price: Number,
+});
+
+const Service = new mongoose.model("Service", serviceSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -17,6 +30,25 @@ app.get("/about", (req, res) => {
 });
 app.get("/services", (req, res) => {
   res.render("services");
+});
+app.get("/createService", (req, res) => {
+  res.render("createService");
+});
+
+app.post("/createService", (req, res) => {
+  const service = new Service({
+    title: req.body.title,
+    body: req.body.body,
+    price: req.body.price,
+  });
+  service.save((err) => {
+    if (err) {
+      console.log("err in post");
+      res.redirect("/createService");
+    } else {
+      res.redirect("services");
+    }
+  });
 });
 
 app.listen(3000, () => {
