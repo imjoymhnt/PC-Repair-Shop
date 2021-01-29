f00cc6bde55bdec2f0fad93ad85be9e962f886b6
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-01-29 21:13:30 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.listen(3000, () => {
+  console.log("Listining to the port 3000");
+});
